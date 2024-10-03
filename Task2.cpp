4f821eb8935f461bd5fdf23edc6e7a06c2f8475a
ayush-pdl <ayush.paudel@students.plymouth.ac.uk>
2024-10-03 15:31:22 +0100

    Create Task2.cpp

---
+++ Task2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "enter any number:";
+  cin >> n;
+  
+  while (n >=0) {
+    cout << "try again" << endl;
+    cin >> n;
+
+  }
+ return 0;   
+}
